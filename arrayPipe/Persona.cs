b7e07ccea724c189ce8d5a9906011e775787639e
Olmeiro Orozco <olmeiro.orozco@gmail.com>
2020-03-13 17:12:10 -0500

    Add project files.

---
+++ arrayPipe/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayPipe
+{
+    class Persona
+    {
+        public string nombre;
+        public int edad;
+
+        public Persona()
+        {
+
+        }
+
+        public Persona(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
---
+++ arrayPipe/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Collections;
+
+namespace arrayPipe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Persona> listaPersonas = new List<Persona>();
+            Persona miPersona = new Persona();
+            //ArrayList listaPersonas = new ArrayList();
+
+
+            listaPersonas.Add(new Persona("Martin", 56));
+            listaPersonas.Add(new Persona("Olmeiro", 89));
+            listaPersonas.Add(new Persona("Felipe", 104));
+            listaPersonas.Add(new Persona("Luisa", 18));
+            listaPersonas.Add(new Persona("Carolina", 25));
+            listaPersonas.Add(new Persona("Donante", 19));
+
+
+            foreach (Persona per in listaPersonas)
+            {
+                Console.WriteLine(per.nombre + " " + per.edad);
+                
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ingrese el nombre de la persona a eliminar");
+            string nombreDel = Console.ReadLine();
+            Console.WriteLine();
+
+            for (var index = listaPersonas.Count - 1; index >= 0; index--)
+            {
+                if (listaPersonas[index].nombre == nombreDel)
+                {
+                    listaPersonas.RemoveAt(index);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Nueva lista");
+            Console.WriteLine();
+
+            listaPersonas.ForEach(
+                per => Console.WriteLine(per.nombre));
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
